d520b742dae687fc67daf5011aeccbb3130ca50c
kimkeunho7890-ux <kimkeunho7890@gmail.com>
2025-09-29 17:27:19 +0900

    Add files via upload

--- public/renderer_admin.js
+++ public/renderer_admin.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', async (e) => {
     const name = document.getElementById('name').value;
     const password = document.getElementById('password').value;
 
-    const response = await fetch('http://localhost:3000/register', {
+    const response = await fetch('/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ userId, name, password })
